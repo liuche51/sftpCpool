b2dd2e745e1adf2ceb7160984acba809fc1aeefa
liuche <liuche51@foxmail.com>
2020-04-01 20:44:03 +0800

    编写测试代码

--- sftpCpool-test/src/test/java/UnitTest.java
+++ sftpCpool-test/src/test/java/UnitTest.java
@@ -1,11 +1,16 @@
 import com.github.liuche51.easyTask.test.Test1;
 import org.junit.Test;
 
+import java.util.List;
+
 public class UnitTest {
     @Test
     public void test1() {
         try {
-            Test1.getSftpListFiles();
+            List<String> list= Test1.getSftpListFiles();
+            for(String s:list){
+                System.out.println(s);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
